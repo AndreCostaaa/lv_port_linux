f21925b9910b13fa8d51bb0723eb52debb13c881
André <andre_miguel_costa@hotmail.com>
2025-05-29 13:08:19 +0200

    feat: render test application

--- src/main.c
+++ src/main.c
@@ -1,125 +1,32 @@
-/*******************************************************************
- *
- * main.c - LVGL simulator for GNU/Linux
- *
- * Based on the original file from the repository
- *
- * @note eventually this file won't contain a main function and will
- * become a library supporting all major operating systems
- *
- * To see how each driver is initialized check the
- * 'src/lib/display_backends' directory
- *
- * - Clean up
- * - Support for multiple backends at once
- *   2025 EDGEMTech Ltd.
- *
- * Author: EDGEMTech Ltd, Erik Tagirov (erik.tagirov@edgemtech.ch)
- *
- ******************************************************************/
-#include <unistd.h>
-#include <pthread.h>
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <render/lv_demo_render.h>
 
-#include "lvgl/lvgl.h"
-#include "lvgl/demos/lv_demos.h"
-
-#include "src/lib/driver_backends.h"
-#include "src/lib/simulator_util.h"
-#include "src/lib/simulator_settings.h"
-
-/* Internal functions */
-static void configure_simulator(int argc, char **argv);
-static void print_lvgl_version(void);
-static void print_usage(void);
-
-/* contains the name of the selected backend if user
- * has specified one on the command line */
-static char *selected_backend;
-
-/* Global simulator settings, defined in lv_linux_backend.c */
-extern simulator_settings_t settings;
-
-
-/**
- * @brief Print LVGL version
- */
-static void print_lvgl_version(void)
+typedef struct
 {
-    fprintf(stdout, "%d.%d.%d-%s\n",
-            LVGL_VERSION_MAJOR,
-            LVGL_VERSION_MINOR,
-            LVGL_VERSION_PATCH,
-            LVGL_VERSION_INFO);
-}
+    const char * name;
+    lv_color_format_t format;
+} color_format_t;
 
-/**
- * @brief Print usage information
- */
-static void print_usage(void)
+void test_render(lv_display_t * display, const color_format_t * format)
 {
-    fprintf(stdout, "\nlvglsim [-V] [-B] [-b backend_name] [-W window_width] [-H window_height]\n\n");
-    fprintf(stdout, "-V print LVGL version\n");
-    fprintf(stdout, "-B list supported backends\n");
-}
+    lv_display_set_color_format(display, format->format);
 
-/**
- * @brief Configure simulator
- * @description process arguments recieved by the program to select
- * appropriate options
- * @param argc the count of arguments in argv
- * @param argv The arguments
- */
-static void configure_simulator(int argc, char **argv)
-{
-    int opt = 0;
-    char *backend_name;
+    lv_opa_t opa_values[2] = {0xff, 0x80};
+    uint32_t opa;
+    for(opa = 0; opa < 2; opa++) {
+        uint32_t i;
+        for(i = 0; i < LV_DEMO_RENDER_SCENE_NUM; i++) {
 
-    selected_backend = NULL;
-    driver_backends_register();
+            /*Skip test with transformed indexed images if they are not loaded to RAM*/
+            if(LV_BIN_DECODER_RAM_LOAD == 0 &&
+               (i == LV_DEMO_RENDER_SCENE_IMAGE_NORMAL_2 || i == LV_DEMO_RENDER_SCENE_IMAGE_RECOLOR_2))
+                continue;
 
-    /* Default values */
-    settings.window_width = atoi(getenv("LV_SIM_WINDOW_WIDTH") ? : "800");
-    settings.window_height = atoi(getenv("LV_SIM_WINDOW_HEIGHT") ? : "480");
+            lv_demo_render(i, opa_values[opa]);
 
-    /* Parse the command-line options. */
-    while ((opt = getopt (argc, argv, "b:fmW:H:BVh")) != -1) {
-        switch (opt) {
-        case 'h':
-            print_usage();
-            exit(EXIT_SUCCESS);
-            break;
-        case 'V':
-            print_lvgl_version();
-            exit(EXIT_SUCCESS);
-            break;
-        case 'B':
-            driver_backends_print_supported();
-            exit(EXIT_SUCCESS);
-            break;
-        case 'b':
-            if (driver_backends_is_supported(optarg) == 0) {
-                die("error no such backend: %s\n", optarg);
-            }
-            selected_backend = strdup(optarg);
-            break;
-        case 'W':
-            settings.window_width = atoi(optarg);
-            break;
-        case 'H':
-            settings.window_height = atoi(optarg);
-            break;
-        case ':':
-            print_usage();
-            die("Option -%c requires an argument.\n", optopt);
-            break;
-        case '?':
-            print_usage();
-            die("Unknown option -%c.\n", optopt);
+            char buf[128];
+            lv_snprintf(buf, sizeof(buf), "draw/render/%s/demo_render_%s_opa_%d.png", format->name,
+                        lv_demo_render_get_scene_name(i), opa_values[opa]);
+            lv_test_screenshot_compare(buf);
         }
     }
 }
@@ -130,32 +37,16 @@ static void configure_simulator(int argc, char **argv)
  * @param argc the count of arguments in argv
  * @param argv The arguments
  */
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
-
-    configure_simulator(argc, argv);
-
-    /* Initialize LVGL. */
+    color_format_t formats[] = {{.name = "rgb565", .format = LV_COLOR_FORMAT_RGB565},
+                                {.name = "rgb888", .format = LV_COLOR_FORMAT_RGB888},
+                                {.name = "xrgb8888", .format = LV_COLOR_FORMAT_XRGB8888}};
     lv_init();
-
-    /* Initialize the configured backend */
-    if (driver_backends_init_backend(selected_backend) == -1) {
-        die("Failed to initialize display backend");
+    lv_display_t * display = lv_test_display_create(1024, 768);
+    for(size_t i = 0; i < sizeof(formats) / sizeof(formats[0]); ++i) {
+        test_render(display, &formats[i]);
     }
-
-    /* Enable for EVDEV support */
-#if LV_USE_EVDEV
-    if (driver_backends_init_backend("EVDEV") == -1) {
-        die("Failed to initialize evdev");
-    }
-#endif
-
-    /*Create a Demo*/
-    lv_demo_widgets();
-    lv_demo_widgets_start_slideshow();
-
-    /* Enter the run loop of the selected backend */
-    driver_backends_run_loop();
 
     return 0;
 }
